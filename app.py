ebe842320049fb6db625bf7dfb4049a8de050a84
Kyle Lewis <kylel9815@gmail.com>
2020-09-04 15:52:08 -0400

    added finished bootstrap website

--- app.py
+++ app.py
@@ -1,48 +1,32 @@
 from flask import Flask, render_template, redirect
+import pymongo
 from flask_pymongo import PyMongo
 from scrape_mars import scrape
 
 app = Flask(__name__)
-mongo =  PyMongo(app, uri="mongodb://localhost:27017/planets")
+conn = 'mongodb://localhost:27017'
 
+client = pymongo.MongoClient(conn)
+db = client.planets
+
+db.mars.drop()
+
+# Test Data
+db.mars.insert_one({"Planet_News": {"title": "Article", "blurb": "blurb"}, "Planet_Facts": {"table": {"Mars": {"key": "value"}}},
+                    "Planet_Image": {"featured_image_url": "https://thumbs-prod.si-cdn.com/PMYKxQ9NTtXkrcWz3tMItcFfcjs=/800x600/filters:no_upscale()/https://public-media.si-cdn.com/filer/mars_img.jpg"},
+                    "Hemispheres": [{"img_url": "https://upload.wikimedia.org/wikipedia/commons/8/8a/Jupiter_family.jpg", "title": "Hemisphere"},
+                    {"img_url": "https://upload.wikimedia.org/wikipedia/commons/7/7b/Io_highest_resolution_true_color.jpg", "title": "Hemisphere2"}]})
 
 @app.route("/")
 def index():
-    planet_data = mongo.planets.Mars.find_one()
+    planet_data = db.mars.find_one()
     return render_template("index.html", planet_data=planet_data)
 
 @app.route("/scrape")
 def scraper():
-    planets = mongo.planets.Mars
     planet_data = scrape()
-    planets.update({}, planet_data, upsert=True)
+    db.mars.update({}, planet_data, upsert=True)
     return redirect("/", code=302)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -5,7 +5,7 @@ import bs4, requests
 def scrape():
     final_dict = {}
     #Mars News
-    mars_news_url = r"https://mars.nasa.gov/news/8744/nasa-engineers-checking-insights-weather-sensors/"
+    mars_news_url = "https://mars.nasa.gov/news/8744/nasa-engineers-checking-insights-weather-sensors/"
     response = requests.get(mars_news_url, "lxml")
     soup = bs4.BeautifulSoup(response.text)
     news_title = soup.select("h1")[0].text
@@ -17,8 +17,8 @@ def scrape():
     final_dict["Planet_News"]["blurb"] = blurb_text
 
     #### JPL Mars Space Images
-    images_url = r"https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-    base_url = r"https://www.jpl.nasa.gov"
+    images_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    base_url = "https://www.jpl.nasa.gov"
     response_img = requests.get(images_url, "lxml")
     soupier = bs4.BeautifulSoup(response_img.text)
     soupier.select(".img")
@@ -32,30 +32,31 @@ def scrape():
 
     ### Mars Facts
 
-    facts_response = requests.get(r"https://space-facts.com/mars/")
-    table = pd.read_html(r"https://space-facts.com/mars/")
+    facts_response = requests.get("https://space-facts.com/mars/")
+    table = pd.read_html("https://space-facts.com/mars/")
     table[0]
     table = table[0]
     table.rename({0: "Facts", 1:"Mars"}, axis=1, inplace=True)
+    table.set_index("Facts", inplace=True)
     table.to_html("table.html")
     
     final_dict["Planet_Facts"] = {}
-    final_dict["Planet_Facts"]["table"] = table.to_html()
+    final_dict["Planet_Facts"]["table"] = table.to_dict()
     
 
     # Hemisphere Requests
-    response_4 = requests.get(r"https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars")
+    response_4 = requests.get("https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars")
     soupabc = bs4.BeautifulSoup(response_4.text, "lxml")
     soupabc.select("h3")
 
-    hemisphere_dict = [{"img_url": r"https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg"},
-                         {"img_url": r"https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg"},
-                         {"img_url": r"https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg"},
-                         {"img_url": r"https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg"}]
+    hemisphere_list = [{"img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg"},
+                         {"img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg"},
+                         {"img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg"},
+                         {"img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg"}]
 
-    for i, hemisphere in enumerate(soupabc.select('h3')):
-        hemisphere_dict[i]["title"] = hemisphere.text
+    for name, dictionary in zip(soupabc.select('h3'), hemisphere_list):
+        dictionary["title"] = name.text
 
-    final_dict["Hemispheres"] = hemisphere_dict
+    final_dict["Hemispheres"] = hemisphere_list
 
     return final_dict
